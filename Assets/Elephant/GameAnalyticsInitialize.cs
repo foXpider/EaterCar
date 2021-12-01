3fc27169c8ea752f07c8126f64e624184b3aafc9
mehmet_akduman <akdumanmehmet2002@gmail.com>
2021-12-01 11:02:25 +0300

    sdk

---
+++ Assets/Elephant/GameAnalyticsInitialize.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameAnalyticsSDK;
+
+public class GameAnalyticsInitialize : MonoBehaviour
+{
+    void Start()
+    {
+        GameAnalytics.Initialize();
+    }
+}
